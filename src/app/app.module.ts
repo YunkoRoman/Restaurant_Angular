eaa11313baebdc931acbe64b12785f6a69867933
YunkoRoman <romanyunko1996@gmail.com>
2019-07-24 23:59:30 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,18 @@ import { AuthComponent } from './auth/auth.component';
 import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  {path:'login', component: AuthComponent}
+  {path:'login', component: AuthComponent},
+  {path:'registration', component: RegistrationComponent},
+
 ] ;
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
