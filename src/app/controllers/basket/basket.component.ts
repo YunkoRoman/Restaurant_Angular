4d3ef0c976fbe0673d2ece995f7e0053679fdff0
YunkoRoman <romanyunko1996@gmail.com>
2019-08-31 22:18:43 +0300

    I am working.

--- src/app/controllers/basket/basket.component.ts
+++ src/app/controllers/basket/basket.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+///<reference path="../../../../node_modules/@angular/core/core.d.ts"/>
+import {Component, OnInit, DoCheck} from '@angular/core';
+import {BasketService} from "../../services/basket.service";
+import {Response} from "../../interfaces/Response";
 
 @Component({
   selector: 'app-basket',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent implements OnInit {
+  public basketProducts: any;
 
-  constructor() { }
-
+  constructor(private BasketService: BasketService) {
+  }
+  // ngDoCheck(): void {
+  //   this.BasketService.readProduct().subscribe((data: Response) => {
+  //     console.log(data.msg);
+  //     this.basketProducts = data.msg;
+  //   })
+  // }
   ngOnInit() {
+    this.BasketService.readProduct().subscribe((data: Response) => {
+      console.log(data.msg);
+      this.basketProducts = data.msg;
+    })
   }
 
+  addProduct(id, quantity) {
+    this.BasketService.additionQuantity(id, quantity).subscribe((data:Response) => {
+      console.log(data.msg);
+    })
+  }
+
+  subtractionProduct() {
+
+  }
 }
--- src/app/controllers/restaurant/restaurant.component.ts
+++ src/app/controllers/restaurant/restaurant.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Response} from "../../interfaces/Response";
 import {RestaurantMenuService} from "../../services/restaurant-menu.service";
+import {BasketService} from "../../services/basket.service";
 
 @Component({
   selector: 'app-restaurant',
@@ -13,9 +14,11 @@ export class RestaurantComponent implements OnInit {
   public menus: any = [];
   public productsObj: any = [];
   public showBlock: boolean = false;
+  public counter: number;
 
   constructor(private route: ActivatedRoute,
-              private RestaurantMenuService: RestaurantMenuService) {
+              private RestaurantMenuService: RestaurantMenuService,
+              private BasketService: BasketService) {
   }
 
   ngOnInit() {
@@ -29,16 +32,19 @@ export class RestaurantComponent implements OnInit {
     })
   }
 
-  // Buy() {
-  //   // Добавляємо замовлення в корзину
-  //
-  // }
+  Buy(product_id) {
+    this.BasketService.addProduct(product_id).subscribe((data: Response) => {
+      console.log(data.msg);
+    })
+  }
 
   UploadProduct(menu_id) {
     this.RestaurantMenuService.UploadProduuct(menu_id).subscribe((data: Response) => {
       this.productsObj = data.msg;
       console.log(data.msg);
       if (data.success === true) this.showBlock = true;
+      this.counter = 1
     })
   }
+
 }
